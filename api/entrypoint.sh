90f3d2d468f13e37f2495639af6342e5b9c4b3ea
groverongo <ugartegrover@gmail.com>
2025-01-23 15:41:35 -0500

    feat: Validate CONDA_ENV_NAME environment variable before activation in entrypoint script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash --login
 # The --login ensures the bash configuration is loaded,
 
+if [ -z "${CONDA_ENV_NAME}" ]; then
+    echo "CONDA_ENV_NAME environment variable is not set."
+    exit 1
+fi
+
 # Temporarily disable strict mode and activate conda:
 set +euo pipefail
-conda activate PexBot-api
+conda activate "${CONDA_ENV_NAME}"
 # enable strict mode:
 set -euo pipefail
 
